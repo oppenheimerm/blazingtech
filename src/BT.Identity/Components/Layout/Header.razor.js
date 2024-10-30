15d3fe584ae800b5d1126057e7ad1083132f1017
Michiel Oppenheimer <michieloppenheimer@outlook.com>
2024-10-30 17:38:41 +0000

    Identity API / Admin added to project

---
+++ src/BT.Identity/Components/Layout/Header.razor.js
@@ -0,0 +1,14 @@
+﻿
+export function toggleSidebar(currentValue) {
+    var element = document.getElementById("sidebar");
+    if (currentValue == 0) {        
+        element.classList.add("-translate-x-full");
+        element.classList.remove("translate-x-0");
+        console.log("Sidebar closed");
+    } else {
+        //  open
+        element.classList.add("translate-x-0");
+        element.classList.remove("-translate-x-full");
+        console.log("Sidebar open");
+    }
+}
